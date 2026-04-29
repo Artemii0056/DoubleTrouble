360df7b0a7c4c8753f5a6754c20dd44669f17d70
Artemii <aza_art@mail.ru>
2026-04-29 14:32:28 +0500

    refactoring

--- Assets/Scripts/Game/Projectiles/Services/ProjectileFactory.cs
+++ Assets/Scripts/Game/Projectiles/Services/ProjectileFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Game.Combat.Damage;
+using Game.Combat.Statuses;
 using Game.Projectiles.Effects;
 using Game.Projectiles.Runtime;
 using UnityEngine;
@@ -9,17 +11,45 @@ namespace Game.Projectiles.Services
     {
         private int _nextId = 1;
 
+        private readonly DamageService _damageService;
+        private readonly StatusEffectService _statusEffectService;
+
+        public ProjectileFactory(
+            DamageService damageService,
+            StatusEffectService statusEffectService)
+        {
+            _damageService = damageService;
+            _statusEffectService = statusEffectService;
+        }
+
         public ProjectileRuntime Create(
             ProjectileConfig config,
             int ownerId,
             Vector3 position,
             Vector3 direction)
         {
-            List<IProjectileEffect> effects = new List<IProjectileEffect>();
-            
-            var projectile = new ProjectileRuntime(
+            var effects = new List<IProjectileEffect>();
+
+            foreach (var effectConfig in config.effects)
+            {
+                switch (effectConfig)
+                {
+                    case MagicDamageEffectConfig magic:
+                        effects.Add(new MagicDamageEffect(magic.damage, _damageService));
+                        break;
+
+                    case SlowEffectConfig slow:
+                        // effects.Add(new SlowEffect(
+                        //     slow.slowPower,
+                        //     slow.duration,
+                        //     _statusEffectService));
+                        break;
+                }
+            }
+
+            return new ProjectileRuntime(
                 _nextId++,
-                 ownerId,
+                ownerId,
                 position,
                 direction.normalized,
                 config.speed,
@@ -28,8 +58,6 @@ namespace Game.Projectiles.Services
                 config.pierce,
                 config.ricochet,
                 effects);
-
-            return projectile;
         }
     }
 }
